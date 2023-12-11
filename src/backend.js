9d3a66dea93a7ce214ab9c0b05a8840c44d2cfde
Tejas Guduru <30982138+gtejas1@users.noreply.github.com>
2023-12-11 11:01:36 -0600

    Update backend.js

--- src/backend.js
+++ src/backend.js
@@ -1,3 +1,3 @@
 // export const API = process.env.REACT_APP_BACKEND;
 export const API =
-  "https://epcmaby96j.execute-api.us-east-1.amazonaws.com/default/ecomm-backend/api";
+  "https://r79b232i3k.execute-api.us-east-1.amazonaws.com/dev/ecomm-backend/api";
