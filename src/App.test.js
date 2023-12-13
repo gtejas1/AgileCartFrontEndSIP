10f72bbf11c69c02986ef83b6b46f4d18de7bd03
Tejas Guduru <tejas.guduru@gmail.com>
2023-12-13 17:49:06 -0600

    Added test case and Updated Readme file

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,18 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import Home from "./core/Home";
 
+test("Test to check website title", () => {
+  render(
+    <Router>
+      <Home />
+    </Router>
+  );
+  const linkElement = screen.getByText(
+    /Welcome to the AgileCart T-shirt store/i
+  );
+  expect(linkElement).toBeInTheDocument();
+});
+
 test("renders centixo T shirt store", () => {
   render(
     <Router>
